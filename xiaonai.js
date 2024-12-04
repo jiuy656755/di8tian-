795f47eeb7352106619d4c2d3bc338908105c9cf
jiuy656755 <86563549+jiuy656755@users.noreply.github.com>
2024-12-04 19:36:18 +0800

    Update xiaonai.js

--- xiaonai.js
+++ xiaonai.js
@@ -1,1 +1,23 @@
+*/
 
+var body = $response.body;
+
+// 替换 "free":\w+
+body = body.replace(/"free":\w+/g, '"free":true');
+
+// 替换 "isSchoolAgeMember":\w+
+body = body.replace(/"isSchoolAgeMember":\w+/g, '"isSchoolAgeMember":true');
+
+// 替换 "isNormalMember":\w+
+body = body.replace(/"isNormalMember":\w+/g, '"isNormalMember":true');
+
+// 替换 "baseMemberAvailable":\w+
+body = body.replace(/"baseMemberAvailable":\w+/g, '"baseMemberAvailable":false');
+
+// 替换 "expiresAt": "\d{4}年\d{1,2}月\d{1,2}日"
+body = body.replace(/"expiresAt": "\d{4}年\d{1,2}月\d{1,2}日"/g, '"expiresAt": "2099年9月9日"');
+
+// 替换 "selectedKid" : {
+body = body.replace(/"selectedKid" : {/g, '"selectedKid" : { "schoolAgeMember" : { "expiresAt" : "2099年9月9日", "__typename" : "Member" },');
+
+$done({ body });
